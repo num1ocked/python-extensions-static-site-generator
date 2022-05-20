4ac13d4b5880077c36cdc309e1f9c4e5794b40ba
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-20 11:37:13 +0100

    9

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -46,8 +46,7 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        filtered = hooks.filter("generate_menu", html, self.base_ext)
-        self.write(path, dest, filtered)
+        self.write(path, dest, html)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
@@ -59,6 +58,7 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
+        filtered = hooks.filter("generate_menu", html, self.base.ext)
         self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
