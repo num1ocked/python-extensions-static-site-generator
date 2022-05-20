466e204c9936d71a796c75868004d5d8a280a212
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-20 10:13:23 +0100

    4 syntax

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -7,6 +7,6 @@ def register(hook, order=0):
     return register_callback
 
 def event(hook, *args):
-    for order in sorted(_callbacks.get(hook, {}))
-    for func in _callbacks[hook][order]
-        func(*args)
+    for order in sorted(_callbacks.get(hook, {})):
+        for func in _callbacks[hook][order]:
+            func(*args)
